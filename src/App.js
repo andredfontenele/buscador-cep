0accde42415beab557305afadf785f2792ce8b2d
André Fontenele <andre.hiip@gmail.com>
2022-12-28 18:44:20 -0300

    Criação do corpo da aplicação e estilização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP"></input>
+        <button className="buttonSearch">
+          <FiSearch size={25} color="FFF"></FiSearch>
+        </button>
+      </div>
+      <main className="main">
+        <h2>Cep: 0000000</h2>
+        <span>Rua teste</span>
+        <span>Complemento: seila</span>
+        <span>Vila rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
     </div>
   )
 }
