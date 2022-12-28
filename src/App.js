45b92a4a8f9d720702acdbee217dbc323c7957eb
André Fontenele <andre.hiip@gmail.com>
2022-12-28 19:40:43 -0300

    API integrada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
+import api from './services/api'
 
 function App() {
   const [input, setInput] = useState('')
+  const [cep, setCEP] = useState('')
 
-  function handleSearch() {
-    alert(input)
+  async function handleSearch() {
+    //01001000/json/
+    if (input === '') {
+      alert('Preencha algum CEP!')
+      return
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setCEP(response.data)
+      setInput('')
+    } catch {
+      alert('Erro ao buscar')
+      setInput('')
+    }
   }
 
   return (
@@ -23,13 +38,18 @@ function App() {
           <FiSearch size={25} color="FFF"></FiSearch>
         </button>
       </div>
-      <main className="main">
-        <h2>Cep: 0000000</h2>
-        <span>Rua teste</span>
-        <span>Complemento: seila</span>
-        <span>Vila rosa</span>
-        <span>Campo Grande - MS</span>
-      </main>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   )
 }
